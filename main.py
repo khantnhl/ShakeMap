a89adc179973f6e450c4088a7fdf3bfd5856a42c
Khant Nyi Hlaing <khantnyihlaingkh@gmail.com>
2025-06-30 15:32:27 -0700

    setup gcloud, vertexai and envs

---
+++ main.py
@@ -0,0 +1,58 @@
+import vertexai
+import os
+from dotenv import load_dotenv
+from vertexai.generative_models import GenerativeModel, GenerationConfig, Image, SafetySetting, HarmCategory, HarmBlockThreshold
+from google.auth.transport.requests import Request
+from google.oauth2.service_account import Credentials
+
+load_dotenv()
+
+path = os.environ['filepath']
+projectID = os.environ['projectID']
+
+credentials = Credentials.from_service_account_file(
+    path, 
+    scopes=["https://www.googleapis.com/auth/cloud-platform"])
+
+if credentials.expired:
+    credentials.refresh(Request())
+
+vertexai.init(project=projectID, location='us-central1', credentials=credentials)
+
+config= GenerationConfig(
+        temperature=0.4,
+        top_p=0.95,
+        top_k=20,
+        candidate_count=1,
+        seed=5,
+        max_output_tokens=8100,
+        stop_sequences=["STOP!"],
+        presence_penalty=0.0,
+        frequency_penalty=0.0,
+        response_logprobs=False,  # Set to True to get logprobs, Note this can only be run once per day
+    )
+
+sf_settings = [
+    SafetySetting(
+        category=HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold=HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    ),
+    SafetySetting(
+        category=HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold=HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    ),
+    SafetySetting(
+        category=HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold=HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    ),
+    SafetySetting(
+        category=HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold=HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    ),
+]
+
+
+
+model = GenerativeModel("gemini-2.5-flash")
+print(model.generate_content(generation_config=config, safety_settings=sf_settings, contents="What is Blue?", stream=False).text)
+
