1281f78faa810835645f07d124c0d22291fe96bd
Khant Nyi Hlaing <khantnyihlaingkh@gmail.com>
2025-07-07 16:58:32 -0700

    organized files, working on MMIretriever

---
+++ config/gemini_config.py
@@ -0,0 +1,36 @@
+import vertexai
+from vertexai.generative_models import GenerationConfig, SafetySetting, HarmCategory, HarmBlockThreshold
+
+geminiConfig= GenerationConfig(
+        temperature=0.4,
+        top_p=0.95,
+        top_k=20,
+        candidate_count=1,
+        seed=5,
+        max_output_tokens=8100,
+        stop_sequences=["STOP!"],
+        presence_penalty=0.0,
+        frequency_penalty=0.0,
+        response_logprobs=False,  # Set to True to get logprobs, Note this can only be run once per day
+        response_mime_type="application/json"
+    )
+
+sf_settings = [
+    SafetySetting(
+        category=HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+        threshold=HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    ),
+    SafetySetting(
+        category=HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold=HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    ),
+    SafetySetting(
+        category=HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold=HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    ),
+    SafetySetting(
+        category=HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+        threshold=HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    ),
+]
+
--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import vertexai
 import os
-import json
 from dotenv import load_dotenv
-from vertexai.generative_models import GenerativeModel, GenerationConfig, Image, SafetySetting, HarmCategory, HarmBlockThreshold, Part
-from google.genai.types import Tool, GenerateContentConfig
-from google.genai import types
+from vertexai.generative_models import GenerativeModel, Part
 from google.auth.transport.requests import Request
 from google.oauth2.service_account import Credentials
-from gcloudUtil import generate_download_signed_url_v4, generate_object_urls
+from services.gcloudUtil import generate_download_signed_url_v4, generate_object_urls
+from config.gemini_config import geminiConfig, sf_settings
 
 load_dotenv()
 
@@ -23,59 +21,16 @@ if credentials.expired:
 
 vertexai.init(project=projectID, location='us-central1', credentials=credentials)
 
-config= GenerationConfig(
-        temperature=0.4,
-        top_p=0.95,
-        top_k=20,
-        candidate_count=1,
-        seed=5,
-        max_output_tokens=8100,
-        stop_sequences=["STOP!"],
-        presence_penalty=0.0,
-        frequency_penalty=0.0,
-        response_logprobs=False,  # Set to True to get logprobs, Note this can only be run once per day
-        response_mime_type="application/json"
-    )
-
-sf_settings = [
-    SafetySetting(
-        category=HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
-        threshold=HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
-    ),
-    SafetySetting(
-        category=HarmCategory.HARM_CATEGORY_HARASSMENT,
-        threshold=HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
-    ),
-    SafetySetting(
-        category=HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-        threshold=HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
-    ),
-    SafetySetting(
-        category=HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
-        threshold=HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
-    ),
-]
-
-url_context_tool = Tool(url_context = types.UrlContext)
-
 model = GenerativeModel("gemini-2.5-flash")
 
+# mandalay
 event_record = {
     "mag" : 7.7,
     "latLng" : [22.011, 95.936]
 }
 
 # OUTPUT Prompts 
-from prompts import location_prompt, output_prompt
-
-# response = model.generate_content(
-#     generation_config=config, 
-#     safety_settings=sf_settings, 
-#     contents=location_prompt,    
-#     stream=False
-#     )
-
-# print(response.text)
+from prompts.prompts import location_prompt, output_prompt
 
 # queue
 signed_urls = generate_object_urls()
@@ -104,7 +59,7 @@ for i, url in enumerate(signed_urls):
 
     try: 
         response = model.generate_content(
-                    generation_config=config, 
+                    generation_config=geminiConfig, 
                     safety_settings=sf_settings, 
                     contents=context_for_gemini,    
                     stream=False
